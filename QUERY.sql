3152424172ffe6fb3eea10b1af4d3be40f269cd7
Abu Solayman Sefat <sefat01625@gmail.com>
2026-06-14 22:01:53 +0600

    upcoming football matches

--- QUERY.sql
+++ QUERY.sql
@@ -72,3 +72,41 @@ VALUES
 (504, 2, 101, NULL, NULL, 150),
 
 (505, 3, 102, 'C-20', 'Pending', 120);
+
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+SELECT
+  match_id,
+  fixture,
+  base_ticket_price
+FROM
+  matches
+WHERE
+  tournament_category = 'Champions League'
+  AND match_status = 'Available';
+
+
+  -- Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive)
+
+SELECT
+  user_id,
+  full_name,
+  email
+FROM
+  users
+WHERE
+  full_name ilike 'Tanvir%'
+  OR full_name ilike '%Haque%'
+
+
+    -- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+SELECT
+  booking_id,
+  user_id,
+  match_id,
+  COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM
+  Bookings
+WHERE
+  payment_status IS NULL;
+
+Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
